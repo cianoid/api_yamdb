1e7f144fa226542f3769f81375aa692651a0b241
Inna <onevesomosti@yandex.ru>
2021-12-14 17:42:35 +0300

    edit full User model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,5 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+USER_ROLES = [
+    ('admin', 'Администратор'),
+    ('moderator', 'Модератор'),
+    ('user', 'Пользователь'),
+]
+
+
 class User(AbstractUser):
-    pass
+    role = models.CharField(
+        'Роль',
+        max_length=20,
+        choices=USER_ROLES,
+        default='user'
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
