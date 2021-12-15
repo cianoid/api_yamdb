f99b39ef16567fde461b5c2fc111d4c44a627440
Igor Shatava <cianoid@yandex.ru>
2021-12-15 22:53:20 +0300

    feat: add models Genre and Title, add __str__ method to models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,5 +2,34 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50)
+    name = models.CharField(
+        max_length=256)
+    slug = models.SlugField(
+        max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=256)
+    slug = models.SlugField(
+        max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=256)
+    year = models.PositiveSmallIntegerField()
+    category = models.ForeignKey(
+        Category, default=0, on_delete=models.SET_DEFAULT,
+        related_name='titles')
+    genre = models.ManyToManyField(
+        Genre, related_name='titles')
+
+    def __str__(self):
+        return self.name
