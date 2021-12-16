87aa7186ebb4256c93301399f38a7dda4a8a6d64
Igor Shatava <cianoid@yandex.ru>
2021-12-16 06:30:36 +0300

    add positional arguments

--- api_yamdb/reviews/management/commands/importdata.py
+++ api_yamdb/reviews/management/commands/importdata.py
@@ -4,6 +4,13 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
     help = 'Imports test data'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'filename', type=str, help='Path to .csv file with data')
+        parser.add_argument(
+            'model', type=str, help='Model associated with data from filename')
+
     def handle(self, *args, **options):
-        pass
+        filename = options.get('filename')
+        model_name = options.get('model')
 
