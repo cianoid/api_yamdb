6d9dcea1a21f7cade92bce916572941a3f4bac4e
Inna <onevesomosti@yandex.ru>
2021-12-21 19:45:38 +0300

    fix permission AdminOnly

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -28,7 +28,7 @@ class AdminOrReadOnlyPermission(permissions.BasePermission):
 
 class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.role == 'admin':
+        if request.user.is_authenticated and request.user.role == 'admin':
             return True
         return False
 
