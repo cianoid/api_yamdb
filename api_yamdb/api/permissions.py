3994dcd1d5452c97b4caa98770e8efbd4f476ca8
Igor Shatava <cianoid@yandex.ru>
2021-12-22 19:57:23 +0300

    fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,16 +1,10 @@
 from rest_framework import permissions, status
 
+
 class AdminOrReadOnlyPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        jwt = request.META.get('HTTP_AUTHORIZATION')
-
-        if jwt is None:
-            raise CustomAPIException(
-                detail='Необходим JWT-токен',
-                status_code=status.HTTP_401_UNAUTHORIZED)
 
         if not request.user.is_authenticated:
             return False
