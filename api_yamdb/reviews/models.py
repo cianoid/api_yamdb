1fbb6360fa54e99b22e649d2d52e2cb8473c96c2
Igor Shatava <cianoid@yandex.ru>
2021-12-22 21:40:59 +0300

    change migrations

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
     slug = models.SlugField(
         max_length=50, unique=True)
 
+    class Meta:
+        ordering = ['pk']
+
     def __str__(self):
         return self.name
 
@@ -20,6 +23,9 @@ class Genre(models.Model):
         max_length=256)
     slug = models.SlugField(
         max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['pk']
 
     def __str__(self):
         return self.name
