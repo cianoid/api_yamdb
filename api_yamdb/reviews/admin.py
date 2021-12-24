a17f2d620224f027de3e0df1896669e0ab53f3ba
Igor Shatava <cianoid@yandex.ru>
2021-12-24 07:35:09 +0300

    improve admin files

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -5,6 +5,11 @@ from django.contrib.admin.sites import AlreadyRegistered
 
 for model in apps.get_models():
     try:
+        if model.__module__ != 'reviews.models':
+            raise Exception
+        if not getattr(model, 'ADMIN_ZONE', True):
+            raise Exception
+
         admin.site.register(model)
-    except AlreadyRegistered:
+    except (AlreadyRegistered, Exception):
         pass
