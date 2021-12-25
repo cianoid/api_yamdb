ed102cf56917973f68302039415f6ad7f8e8f8e7
Igor Shatava <cianoid@yandex.ru>
2021-12-25 10:58:05 +0300

    fix PEP8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,9 +1,8 @@
-from datetime import datetime
-
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
-from reviews.models import Category, Comment, Genre, Review, Title, TitlesGenre
+
+from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
 
