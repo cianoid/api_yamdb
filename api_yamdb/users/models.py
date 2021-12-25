5e8eb29bcbd2f1e8c0192958e75ef8c977e8033d
Igor Shatava <cianoid@yandex.ru>
2021-12-25 22:15:41 +0300

    fix

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,37 +2,43 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class UserRoles:
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
+    USER = 'user'
+
+    ADMIN_NAME = 'Администратор'
+    MODERATOR_NAME = 'Модератор'
+    USER_NAME = 'Пользователь'
+
+
 class User(AbstractUser):
     """Модель пользователя"""
-    USER = 'user'
-    MODERATOR = 'moderator'
-    ADMIN = 'admin'
-
     ROLE_CHOICES = [
-        (ADMIN, 'Администратор'),
-        (MODERATOR, 'Модератор'),
-        (USER, 'Пользователь')
+        (UserRoles.ADMIN, UserRoles.ADMIN_NAME),
+        (UserRoles.MODERATOR, UserRoles.MODERATOR_NAME),
+        (UserRoles.USER, UserRoles.USER_NAME)
     ]
 
     email = models.EmailField(unique=True)
     role = models.CharField(
         max_length=16,
         choices=ROLE_CHOICES,
-        default=USER
+        default=UserRoles.USER
     )
     bio = models.TextField(blank=True)
 
     class Meta:
-        ordering = ['pk']
+        ordering = ('pk',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
     @property
     def is_moderator(self):
         """Проверяет наличие роли Модератор."""
-        return self.role == 'moderator'
+        return self.role == UserRoles.MODERATOR
 
     @property
     def is_admin(self):
         """Проверяет аличие роли Администратор."""
-        return self.role == 'admin'
+        return self.role == UserRoles.ADMIN
