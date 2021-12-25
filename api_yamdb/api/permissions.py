c68ae41bb39a3cfe4ba784d90c27c5c7d5b8dd8e
Igor Shatava <cianoid@yandex.ru>
2021-12-25 22:12:35 +0300

    fix style

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -26,7 +26,6 @@ class AdminOrSuperuser(permissions.BasePermission):
 
 
 class AuthorOrReadOnly(permissions.BasePermission):
-
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated)
